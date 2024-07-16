c9a9d151e73ad9458702b03cea0c3f2a531bc4d3
ChaosCrider <ChaosCrider@hotmail.com>
2024-07-16 14:51:56 -0400

    Initial commit -  Template project from spring boot

---
+++ src/main/java/com/BdeB/StockVisioBackEnd/StockVisioBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.BdeB.StockVisioBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockVisioBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockVisioBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BdeB/StockVisioBackEnd/StockVisioBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BdeB.StockVisioBackEnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockVisioBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
