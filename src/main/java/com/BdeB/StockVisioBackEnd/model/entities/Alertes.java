d6fa58a03f2b1e2b927b748cc61507153c24fbae
Cherif2024 <cherifmanel.ca@gmail.com>
2024-08-22 09:36:54 -0400

    premier commit de la branche

--- src/main/java/com/BdeB/StockVisioBackEnd/model/entities/Alertes.java
+++ src/main/java/com/BdeB/StockVisioBackEnd/model/entities/Alertes.java
@@ -41,6 +41,79 @@ public class Alertes {
 
     // Getters and setters
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getDateCreation() {
+        return dateCreation;
+    }
+
+    public void setDateCreation(Date dateCreation) {
+        this.dateCreation = dateCreation;
+    }
+
+    public Produit getProduit() {
+        return produit;
+    }
+
+    public void setProduit(Produit produit) {
+        this.produit = produit;
+    }
+
+    public double getSeuilDeclenchement() {
+        return seuilDeclenchement;
+    }
+
+    public void setSeuilDeclenchement(double seuilDeclenchement) {
+        this.seuilDeclenchement = seuilDeclenchement;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getStatut() {
+        return statut;
+    }
+
+    public void setStatut(String statut) {
+        this.statut = statut;
+    }
+
+    public Date getDateResolution() {
+        return dateResolution;
+    }
+
+    public void setDateResolution(Date dateResolution) {
+        this.dateResolution = dateResolution;
+    }
+
+    public String getPriorite() {
+        return priorite;
+    }
+
+    public void setPriorite(String priorite) {
+        this.priorite = priorite;
+    }
+
+
     // Constructor
     public Alertes() {
         this.dateCreation = new Date(); // Default value for dateCreation
